f7b6107ff69a6fc756e5607af1db3537ed6f0bb7
Crich83 <57454470+Crich83@users.noreply.github.com>
2019-11-13 15:31:10 -0500

    Add files via upload
    activity assignment for JavaScript class

---
+++ activity.js
@@ -0,0 +1,22 @@
+var firstnum=prompt("Enter first number")
+var secondnum=prompt("Enter second number")
+var firstcalc = (+firstnum) + (+secondnum);
+var secondcalc = firstnum * secondnum
+var thirdcalc = firstcalc * 3
+document.write("---------Two Intergers-------");
+document.write("<br> Your first number = " + firstnum);
+document.write("<br> Your second number = " + secondnum);
+document.write("<br> ----------------------------");
+document.write("<br><br>---------Calc Sum---------");
+document.write("<br> Your calculated sum = " + firstcalc);
+document.write("<br> ----------------------------");
+document.write("<br><br>------Multiplied--------");
+document.write("<br> Your multiplied ints = " + secondcalc);
+document.write("<br> ----------------------------");
+document.write("<br><br>------Tripled sum--------");
+document.write("<br> Your tripled sum = " + thirdcalc);
+document.write("<br> ----------------------------");
+
+
+
+
