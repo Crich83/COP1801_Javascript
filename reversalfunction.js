7d6f51ad995d70108b1a96578a9666cf00b2726d
Crich83 <57454470+Crich83@users.noreply.github.com>
2019-11-20 10:55:22 -0500

    Add files via upload

---
+++ reversalfunction.js
@@ -0,0 +1,7 @@
+var reversenum=prompt("Enter number to be reversed")
+function reverseNumber(n)
+{
+	n = n + "";
+	return n.split("").reverse().join("");
+}
+document.write(reverseNumber(reversenum));
